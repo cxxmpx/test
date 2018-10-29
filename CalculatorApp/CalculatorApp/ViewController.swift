bf8b96c0df526107fa0889b9ea27447b44365f7d
cxxmpx <ymgchrs@gmail.com>
2018-10-29 21:18:29 +0900

    計算機を作ってみた

---
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by HiroshiYamaguchi on 2018/10/29.
+//  Copyright © 2018 HiroshiYamaguchi. All rights reserved.
+//
+
+import UIKit
+import Expression
+
+class ViewController: UIViewController {
+    @IBOutlet weak var formulLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // ビューがロードされた時点で式と答えのラベルは空にする
+        formulLabel.text = ""
+        answerLabel.text = ""
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func inputFormula(_ sender: UIButton) {
+        // ボタン（Cと=以外）が押されたら式を表示する
+        guard let formulaText = formulLabel.text else {
+            return
+        }
+        guard let senderedText = sender.titleLabel?.text else {
+            return
+        }
+        formulLabel.text = formulaText + senderedText
+    }
+    
+    @IBAction func clearCalculation(_ sender: UIButton) {
+        // Cボタンが押されたら式と答えをクリアする
+        formulLabel.text = ""
+        answerLabel.text = ""
+    }
+    
+    @IBAction func calculateAnswer(_ sender: UIButton) {
+        // =ボタンが押されたら答えを計算して表示する
+        guard let formulaText = formulLabel.text else {
+            return
+        }
+        let formula: String = formatFormula(formulaText)
+        answerLabel.text = evalFormula(formula)
+    }
+    
+    private func formatFormula(_ formula: String) -> String {
+        // 入力された整数には`.0`を追加して小数として評価する
+        // また`÷`を`/`に、`×`を`*`に置換する
+        let formattedFormula: String = formula.replacingOccurrences(
+            of: "(?<=^|[÷×\\+\\-\\(])([0-9]+)(?=[÷×\\+\\-\\)]|$)",
+            with: "$1.0",
+            options: NSString.CompareOptions.regularExpression,
+            range: nil
+            ).replacingOccurrences(of: "÷", with: "/").replacingOccurrences(of: "×", with: "*")
+        return formattedFormula
+    }
+    
+    private func evalFormula(_ formula: String) -> String {
+        do {
+            // Expressionで文字列の計算式を評価して答えを求める
+            let expression = Expression(formula)
+            let answer = try expression.evaluate()
+            return formatAnswer(String(answer))
+        } catch {
+            // 計算式が不当だった場合
+            return "式を正しく入力してください"
+        }
+    }
+    
+    private func formatAnswer(_ answer: String) -> String {
+        // 答えの小数点以下が`.0`だった場合は、`.0`を削除して答えを整数で表示する
+        let formattedAnswer: String = answer.replacingOccurrences(
+            of: "\\.0$",
+            with: "",
+            options: NSString.CompareOptions.regularExpression,
+            range: nil)
+        return formattedAnswer
+    }
+}
+
